10e2786ad1ab6b6c53f97ee7a580baca974cac2f
Jonathan Gamble <jdgamble555@gmail.com>
2024-04-30 20:03:06 -0500

    created

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -9,21 +9,24 @@ import { readable, type Subscriber } from "svelte/store";
 import { auth } from "./firebase";
 import { useSharedStore } from "./use-shared";
 
-export const loginWithGoogle = async () => await signInWithPopup(auth, new GoogleAuthProvider());
+export const loginWithGoogle = async () =>
+    await signInWithPopup(auth, new GoogleAuthProvider());
 export const logout = async () => await signOut(auth);
 
-const user = (defaultUser: UserType | null = null) => readable<UserType | null>(
-    defaultUser,
-    (set: Subscriber<UserType | null>) => {
-        return onIdTokenChanged(auth, (_user: User | null) => {
-            if (!_user) {
-                set(null);
-                return;
-            }
-            const { displayName, photoURL, uid, email } = _user;
-            set({ displayName, photoURL, uid, email });
-        });
-    }
-);
+const user = (defaultUser: UserType | null = null) =>
+    readable<UserType | null>(
+        defaultUser,
+        (set: Subscriber<UserType | null>) => {
+            return onIdTokenChanged(auth, (_user: User | null) => {
+                if (!_user) {
+                    set(null);
+                    return;
+                }
+                const { displayName, photoURL, uid, email } = _user;
+                set({ displayName, photoURL, uid, email });
+            });
+        }
+    );
 
-export const useUser = (defaultUser: UserType | null = null) => useSharedStore('user', user, defaultUser);
+export const useUser = (defaultUser: UserType | null = null) =>
+    useSharedStore('user', user, defaultUser);
