0c71ec99d5a8027affe67e2441af7526657cbb1e
Jonathan Gamble <jdgamble555@gmail.com>
2024-06-29 18:18:16 -0500

    test

--- src/lib/about.ts
+++ src/lib/about.ts
@@ -1,21 +1,22 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { doc, getDoc, getFirestore } from "firebase/firestore/lite";
-//import { initializeServerApp } from "firebase/app";
-import { app } from "./firebase";
+import { initializeServerApp } from "firebase/app";
+import { PUBLIC_FIREBASE_CONFIG } from "$env/static/public";
 
 type AboutDoc = {
     name: string;
     description: string;
 };
 
+const firebase_config = JSON.parse(PUBLIC_FIREBASE_CONFIG);
+
 
 export const getAbout = async (id: string) => {
 
-    /*const serverApp = initializeServerApp(firebase_config, {
+    const serverApp = initializeServerApp(firebase_config, {
         authIdToken: id
-    });*/
+    });
 
-    const db = getFirestore(app);
+    const db = getFirestore(serverApp);
 
     const aboutSnap = await getDoc(
         doc(db, '/about/ZlNJrKd6LcATycPRmBPA')
