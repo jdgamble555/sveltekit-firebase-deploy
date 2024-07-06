9d5c07eb63ba3e237d35bc8dc97e7c8df657ed2d
Jonathan Gamble <jdgamble555@gmail.com>
2024-07-06 17:34:50 -0500

    test

--- src/lib/firebase-lite.ts
+++ src/lib/firebase-lite.ts
@@ -1,7 +1,7 @@
 import { PUBLIC_FIREBASE_CONFIG } from "$env/static/public";
 import { error } from "@sveltejs/kit";
-import { initializeServerApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { FirebaseError, initializeServerApp } from "firebase/app";
+import { getAuth, type Auth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 
 const firebase_config = JSON.parse(PUBLIC_FIREBASE_CONFIG);
@@ -11,23 +11,27 @@ export const firebaseServer = async (request: Request) => {
 
     const authIdToken = request.headers.get('Authorization')?.split('Bearer ')[1];
 
-    console.log('token', authIdToken);
-
     if (!authIdToken) {
         error(401, 'Not Logged In!');
     }
-
 
     const serverApp = initializeServerApp(firebase_config, {
         authIdToken
     });
 
     // auth
-    const serverAuth = getAuth(serverApp);
-    await serverAuth.authStateReady();
+
+    let serverAuth!: Auth;
+    try {
+        serverAuth = getAuth(serverApp);
+        await serverAuth.authStateReady();
+    } catch (e) {
+        if (e instanceof FirebaseError) {
+           error(401, e.message); 
+        }        
+    }
 
     if (serverAuth.currentUser === null) {
-        console.log(serverAuth.currentUser);
         error(401, 'Invalid Token');
     }
 
